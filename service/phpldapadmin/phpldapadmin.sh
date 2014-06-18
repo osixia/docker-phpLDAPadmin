42e9dd99d80c21f96bee9a1c99f4d787ce1d4fd2
Bertrand Gouny <bgouny@hi-media.com>
2014-06-18 18:12:56 +0200

    debut

--- service/phpldapadmin/phpldapadmin.sh
+++ service/phpldapadmin/phpldapadmin.sh
@@ -1,18 +1,46 @@
 #!/bin/sh
 
 # -e Exit immediately if a command exits with a non-zero status
-# -u Treat unset variables as an error when substituting
-set -eu
+set -e
 
 status () {
   echo "---> ${@}" >&2
 }
 
-set -x
-: LDAP_HOST=${LDAP_HOST}
-: LDAP_BASE_DN=${LDAP_BASE_DN}
-: LDAP_LOGIN_DN=${LDAP_LOGIN_DN}
-: LDAP_SERVER_NAME=${LDAP_SERVER_NAME}
+getBaseDn () {
+  IFS="."
+  export IFS
+
+  domain=$1
+  init=1
+
+  for s in $domain; do
+    dc="dc=$s"
+    if [ "$init" -eq 1 ]; then
+      baseDn=$dc
+      init=0
+    else
+      baseDn="$baseDn,$dc" 
+    fi
+  done
+}
+
+# a ldap container is linked to this phpLDAPadmin container
+if [ -n "${LDAP_NAME}" ]; then
+  LDAP_HOST=ldap
+  
+  # Get base dn from ldap domain
+  getBaseDn LDAP_ENV_LDAP_DOMAIN
+
+  LDAP_BASE_DN=$baseDn
+  LDAP_LOGIN_DN="cn=admin,$baseDn"
+  LDAP_SERVER_NAME=${LDAP_ENV_LDAP_ORGANISATION}
+else
+  LDAP_HOST=${LDAP_HOST}
+  LDAP_BASE_DN=${LDAP_BASE_DN}
+  LDAP_LOGIN_DN=${LDAP_LOGIN_DN}
+  LDAP_SERVER_NAME=${LDAP_SERVER_NAME}
+fi
 
 if [ ! -e /etc/phpldapadmin/docker_bootstrapped ]; then
   status "configuring LDAP for first run"
