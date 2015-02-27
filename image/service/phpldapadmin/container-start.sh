abd0b29605da158fd8a185ddff418496725cbbfd
Bertrand Gouny <bgouny@hi-media.com>
2015-02-27 12:04:17 +0100

    config

--- image/service/phpldapadmin/container-start.sh
+++ image/service/phpldapadmin/container-start.sh
@@ -22,13 +22,87 @@ if [ ! -e "$FIRST_START_DONE" ]; then
     a2ensite phpldapadmin
   fi
 
-  get_salt () {
+  get_salt() {
     salt=$(</dev/urandom tr -dc '1324567890#<>,()*.^@$% =-_~;:|{}[]+!`azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN' | head -c64 | tr -d '\\')
   }
 
   # phpLDAPadmin cookie secret
   get_salt
   sed -i "s/blowfish'] = '/blowfish'] = '${salt}/g" /osixia/phpldapadmin/config.php
+
+  print_by_php_type() {
+
+    if [ "$1" == "True" ]; then
+      echo "true"
+    elif [ "$1" == "False" ]; then
+      echo "false"
+    elif [[ "$1" == array\(\'* ]]; then 
+      echo "$1"
+    else
+      echo "'$1'"
+    fi
+  }
+
+  # phpLDAPadmin servers config
+  host_infos() { 
+
+    local to_print=$1
+    local infos=(${!2})
+
+    for info in "${infos[@]}"
+    do
+      host_infos_value "$to_print" "$info"
+    done
+  }
+
+  host_infos_value(){
+
+    local to_print=$1
+    local info_key_value=(${!2})
+
+    local key=${!info_key_value[0]}
+    local value=(${!info_key_value[1]})
+
+    local value_of_value_table=(${!value})
+
+    # it's a table of values
+    if [ "${#value[@]}" -gt "1" ]; then
+      host_infos "$to_print'$key'," "${info_key_value[1]}"
+
+    # the value of value is a table
+    elif [ "${#value_of_value_table[@]}" -gt "1" ]; then
+      host_infos_value "$to_print'$key'," "$value"
+
+    # it's just a not empty value
+    elif [ -n "$value" ]; then
+      local php_value=$(print_by_php_type $value)
+      echo "\$servers->setValue($to_print'$key',$php_value);" >> /osixia/phpldapadmin/config.php
+    fi
+  }
+
+  # phpLDAPadmin config
+  LDAP_HOSTS=($LDAP_HOSTS)
+  for host in "${LDAP_HOSTS[@]}"
+  do
+    
+    #host var contain a variable name, we access to the variable value and cast it to a table
+    infos=(${!host})
+
+    echo "\$servers->newServer('ldap_pla');" >> /osixia/phpldapadmin/config.php
+
+    # it's a table of infos
+    if [ "${#infos[@]}" -gt "1" ]; then
+      echo "\$servers->setValue('server','name','${!infos[0]}');" >> /osixia/phpldapadmin/config.php
+      echo "\$servers->setValue('server','host','${!infos[0]}');" >> /osixia/phpldapadmin/config.php
+      host_infos "" ${infos[1]}
+
+    # it's just a host name
+    else
+      echo "\$servers->setValue('server','name','${!host}');" >> /osixia/phpldapadmin/config.php
+      echo "\$servers->setValue('server','host','${!host}');" >> /osixia/phpldapadmin/config.php
+    fi
+  done
+
 
   # Fix file permission
   find /var/www/ -type d -exec chmod 755 {} \;
